df8e9bc2a422f595f72bbd01e2746f752e6d0717
Muhannad Melaifi <a1417@hotmail.com>
2024-02-11 20:14:41 +0300

    Sorter 2

--- src/Sorter.java
+++ src/Sorter.java
@@ -2,4 +2,19 @@ public class Sorter {
     public static void main(String[] args) {
 
     }
+    public static void selectionSort(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = array[minIndex];
+            array[minIndex] = array[i];
+            array[i] = temp;
+        }
+    }
+
 }
+
