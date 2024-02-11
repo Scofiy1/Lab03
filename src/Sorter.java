847cac79945023a06603fbedd963bdce3c1ca128
Muhannad Melaifi <a1417@hotmail.com>
2024-02-11 21:42:47 +0300

    t

--- src/Sorter.java
+++ src/Sorter.java
@@ -1,7 +1,8 @@
 public class Sorter {
     public static void main(String[] args) {}
        
-public static void insertionSort(int[] array) {
+public static void insertionSort(int n) {
+    int[] array = new int[n];
     for (int i = 1; i < array.length; i++) {
         int key = array[i];
         int j = i - 1;
@@ -13,10 +14,25 @@ public static void insertionSort(int[] array) {
     }
 }
 
-public static void sort(int[] array) {
-	insertionSort(array);
+    public static void sort ( int array){
+        insertionSort(array);
+    }
+    public static void selectionSort ( int[] array){
+        for (int i = 0; i < array.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = array[minIndex];
+            array[minIndex] = array[i];
+            array[i] = temp;
+        }
+    }
+
+    public static void sort ( int[] array){
+        selectionSort(array);
+    }
+
 }
-
-
-}
-
