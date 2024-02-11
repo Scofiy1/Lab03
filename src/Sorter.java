78d8bf93348b67b83bb0e2c8746af4a3b551da19
1OSDD <1OSDD77@GMAIL.COM>
2024-02-11 21:23:56 +0300

    sort method

--- src/Sorter.java
+++ src/Sorter.java
@@ -1,40 +1,22 @@
 public class Sorter {
-    public static void main(String[] args) {
+    public static void main(String[] args) {}
        
-
-	
-	public static void InsertionSort(int arr[])
-	{
-		int n = arr.length;
-		for (int i = 1; i < n; ++i) {
-			int key = arr[i];
-			int j = i - 1;
-
-		
-			while (j >= 0 && arr[j] > key) {
-				arr[j + 1] = arr[j];
-				j = j - 1;
-			}
-			arr[j + 1] = key;
-		}
-	}
-
-
-    }
-    public static void selectionSort(int[] array) {
-        for (int i = 0; i < array.length - 1; i++) {
-            int minIndex = i;
-            for (int j = i + 1; j < array.length; j++) {
-                if (array[j] < array[minIndex]) {
-                    minIndex = j;
-                }
-            }
-            int temp = array[minIndex];
-            array[minIndex] = array[i];
-            array[i] = temp;
+public static void insertionSort(int[] array) {
+    for (int i = 1; i < array.length; i++) {
+        int key = array[i];
+        int j = i - 1;
+        while (j >= 0 && array[j] > key) {
+            array[j + 1] = array[j];
+            j--;
         }
+        array[j + 1] = key;
     }
 }
 
+public static void sort(int[] array) {
+	insertionSort(array);
+}
 
+
+}
 
